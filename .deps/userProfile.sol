2bc3128f75de43c6ed0873b137b9b8ff89ec1573
ruthfabian <freelancenwriter@gmail.com>
2025-07-21 22:12:16 +0100

    hi

--- .deps/userProfile.sol
+++ .deps/userProfile.sol
@@ -1,15 +1,23 @@
 //SPDX-License-Identifier:MIT
 pragma solidity 0.8.30;
 
-contract userProfile{
+contract UserProfile{
     // struct User
     struct User {
         string name;
         uint256 age;
         string email;
+        uint256 registrationTimestamp;
         bool isRegistered;
     }
 
+    //mapping
     //state variable 
+    mapping (address => User) public users;
+    mapping (address => bool) public isRegistered;
+
+    function register(string memory name, uint256 age, string memory email) public {
+        
+    }
 
 }
