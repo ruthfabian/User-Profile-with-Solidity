05b66a6acc4a1f5e3d01bf1169d856aaea1857dd
ruthfabian <freelancenwriter@gmail.com>
2025-07-21 21:10:32 +0100

    com

---
+++ .deps/userProfile.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.30;
+
+contract userProfile{
+    // struct User
+    struct User {
+        string name;
+        uint256 age;
+        string email;
+        bool isRegistered;
+    }
+
+    //state variable 
+
+}
