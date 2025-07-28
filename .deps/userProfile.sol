999cda699e978daee5f837222fd5925bf49e275d
ruthfabian <freelancenwriter@gmail.com>
2025-07-28 18:18:56 +0100

    update

--- .deps/userProfile.sol
+++ .deps/userProfile.sol
@@ -17,7 +17,7 @@ contract UserProfile{
     mapping (address => bool) public isRegistered;
 
     function register(string memory name, uint256 age, string memory email) public {
-        require(!isRegisterd[msg.sender], "User is already registered");
+        require(isRegistered[msg.sender], "User is already registered");
 
         users[msg.sender] = User(name, age, email, block.timestamp, true);
         isRegistered[msg.sender]=true;
